3667a805db5ef97ff2a4a3eab2891a7c03b26bc4
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2022-01-25 12:57:14 +0530

    Make app to work as agent on MacOS

--- Shared/BatteryManagerApp.swift
+++ Shared/BatteryManagerApp.swift
@@ -10,11 +10,40 @@ import SwiftUI
 @main
 struct BatteryManagerApp: App {
     let persistenceController = PersistenceController.shared
-
+    
+#if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        Settings {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+#else
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
+#endif
 }
+
+#if os(macOS)
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBar: StatusBarController?
+    var popover = NSPopover.init()
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentView = ContentView()
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        statusBar = StatusBarController.init(popover)
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+#endif
